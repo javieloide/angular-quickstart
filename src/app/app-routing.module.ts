5fe8284a2d3018bdb3ee006e48c16342c84ba278
javieloide <javi.acba@gmail.com>
2022-08-27 19:32:19 +0200

    Cambios  9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full' }
+  {path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
