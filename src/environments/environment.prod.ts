e03f4a34aaf73e1a58fb9cfdebb9a750fb550cac
javieloide <javi.acba@gmail.com>
2022-08-30 21:01:14 +0200

    Cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://api.weatherlink.com/v2',
   apiKey: 'st19echieqzz9locx4sz17rbfvbupnej',
   secretKey: 'trr8kdnrlzl05ej0hck0wfh1wuqmhldq',
-  localApi: 'https://netlify-example-app.netlify.app/.netlify/functions/api'
+  localApi: 'https://weather-uex.netlify.app/.netlify/functions/api'
 };
